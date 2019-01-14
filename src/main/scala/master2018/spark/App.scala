395305d0dfa1e03882763be4d4c70a1eb3b84ba5
franborat <f.losadadelarosa@gmail.com>
2019-01-14 18:59:07 +0100

    Trying a small spark app to certify its working: count lines containing a,b

--- src/main/scala/master2018/spark/App.scala
+++ src/main/scala/master2018/spark/App.scala
@@ -3,13 +3,32 @@ package master2018.spark
 /**
  * @author ${user.name}
  */
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.log4j.{Level, Logger}
+
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+//  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+//  
+//  def main(args : Array[String]) {
+//    println( "Hello World!" )
+//    println("concat arguments = " + foo(args))
+//  }
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
+    
+    Logger.getLogger("org").setLevel(Level.WARN)
+
+    val conf = new SparkConf().setAppName("My first Spark application")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile("file:///home/fran/spark/DelayTimePredictor/98.txt")
+    val numAs = data.filter(line => line.contains("a")).count()
+    val numBs = data.filter(line => line.contains("b")).count()
+
+    println(s"Lines with a: ${numAs}, Lines with b: ${numBs}")
+    
+}
 
 }
