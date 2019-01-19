cf8119f45506b4668f9a6252cb1417492cfefb37
franborat <f.losadadelarosa@gmail.com>
2019-01-20 00:56:23 +0100

    Revert "building bestParamsModel and showModelMetrics methods in ParametersTuningPipeline"
    This reverts commit 9c3e39e564241afbfef949076cab5001d1b87664.

--- src/main/scala/master2018/spark/pipelines/ParametersTuningPipeline.scala
+++ src/main/scala/master2018/spark/pipelines/ParametersTuningPipeline.scala
@@ -1,15 +1,9 @@
 package master2018.spark.pipelines
 
 import org.apache.spark.ml.{Pipeline, PipelineModel, PipelineStage}
-import org.apache.spark.ml.Estimator
 import org.apache.spark.ml.PipelineModel
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.Dataset
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
-import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
-import org.apache.spark.ml.Model
-import org.apache.spark.ml.tuning.CrossValidatorModel
 
 
 abstract class ParametersTuningPipeline {
@@ -18,36 +12,6 @@ abstract class ParametersTuningPipeline {
   
   val (estimator, paramGrid) = getEstimatorAndParams
   
-  // Method bestParamsModel: Returns the best model after doing the cross validation gridSearch
-  def bestParamsModel(training: Dataset[_]): (CrossValidatorModel) = {
-    
-  val pipeline = new Pipeline()
-  .setStages(Array(estimator))
   
-  // Define CrossValidator
-  val cv = new CrossValidator()
-  .setEstimator(pipeline)
-  .setEvaluator(new BinaryClassificationEvaluator)
-  .setEstimatorParamMaps(paramGrid)
-  .setNumFolds(5)
   
-  // Run cross-validation, and choose the best set of parameters.
-  val cvModel = cv.fit(training)
-  
-  cvModel
-    
-  }
-  
-  //Method compareModelMetrics: Shows the best model metrics and score
-  def showModelMetrics(cvModel: CrossValidatorModel, cvModel2: CrossValidatorModel): Unit = {
-    
-    val avgMetrics = cvModel.avgMetrics
-    val avgMetrics2 = cvModel.avgMetrics
-
-    cvModel.getEstimatorParamMaps.zip(avgMetrics).zipWithIndex.foreach {
-      case ((params, metric), index) =>
-        print(s"Model ${index + 1}:\n$params -> value = $metric")
-    }
-}
-
 }
