f231be82fe39a5a49e68e3a23ec93dcae08aafae
franborat <f.losadadelarosa@gmail.com>
2019-01-20 00:59:36 +0100

    Revert "Data selection and loading."
    This reverts commit f1688eb671812364ff960182bc0ef294d6a47588.

--- src/main/scala/master2018/spark/utils/DataPreparation.scala
+++ src/main/scala/master2018/spark/utils/DataPreparation.scala
@@ -1,42 +1,7 @@
 package master2018.spark.utils
 
-import org.apache.log4j.{LogManager, Logger}
-import org.apache.spark.sql.Dataset
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.SparkSession
-
 class DataPreparation {
   
-  val logger = LogManager.getLogger("org")
   
-  val spark = SparkSession
-                .builder()
-                .appName("Spark SQL")
-                .config("some option", "value")
-                .enableHiveSupport()
-                .getOrCreate()
-                
-   import spark.implicits._         
-   def select (data: Dataset[_]): Dataset[_] = {
-    
-    logger.info("Selecting the variables that are going to be used")
-    
-    // Remove the forbidden variables, except diverted (which has to be used for filtering diverted flights)
-    val forbidden = Seq ( "ArrTime", "ActualElapsedTime", "AirTime", "TaxiIn","CarrierDelay",
-        "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay")
-    logger.info(s"Removing the forbidden variables: ${forbidden.mkString(",")}")
-    var dataset = data.drop(forbidden: _*)
-    
-    // Filter out cancelled and diverted flights
-    dataset = dataset.filter($"Diverted" === 0)
-    dataset = dataset.filter($"Cancelled" === 0).drop("Cancelled", "CancellationCode")
-    
-    // Now remove "Diverted" variable.
-    dataset = data.drop("Diverted")
-    
-    
-    
-    
-    dataset
-  }
+  
 }
